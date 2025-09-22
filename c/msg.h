81ef59a9844f6163d078355805b479d886a70000
Rob Donnelly <rfdonnelly@gmail.com>
2025-09-21 18:22:23 -0700

    feat: better formatting

--- c/msg.h
+++ c/msg.h
@@ -126,7 +126,7 @@ void ntoh_ptp_sync_msg(struct ptp_sync_msg* msg) {
     ntoh_timestamp(&msg->originTimeStamp);
 }
 
-void print_ptp_header(struct ptp_header* hdr) {
+void print_ptp_header(const char* prefix, struct ptp_header* hdr) {
     uint64_t clockIdentity =
           (uint64_t)hdr->portIdentity.clockIdentity.id[7] << 0
         | (uint64_t)hdr->portIdentity.clockIdentity.id[6] << 8
@@ -136,14 +136,35 @@ void print_ptp_header(struct ptp_header* hdr) {
         | (uint64_t)hdr->portIdentity.clockIdentity.id[2] << 40
         | (uint64_t)hdr->portIdentity.clockIdentity.id[1] << 48
         | (uint64_t)hdr->portIdentity.clockIdentity.id[0] << 56;
-    printf("header: { messageType: 0x%x majorSdoId: 0x%x versionPtp: 0x%x minorVersionPtp: 0x%x messageLength: 0x%0x domainNumber: 0x%0x minorSdoId: 0x%x flags: 0x%0x%0x correctionField: %ld messageTypeSpecific: 0x%0x clockIdentity: 0x%0lx portNumber: 0x%0x sequenceId: %d controlField: 0x%0x logMessagePeriod: %d }", hdr->messageType, hdr->majorSdoId, hdr->versionPtp, hdr->minorVersionPtp, hdr->messageLength, hdr->domainNumber, hdr->minorSdoId, hdr->flags[1], hdr->flags[0], hdr->correctionField, hdr->messageTypeSpecific, clockIdentity, hdr->portIdentity.portNumber, hdr->sequenceId, hdr->controlField, hdr->logMessagePeriod);
+    printf("%sheader:\n", prefix);
+    printf("%s  messageType: 0x%x\n", prefix,hdr->messageType);
+    printf("%s  majorSdoId: 0x%x\n", prefix, hdr->majorSdoId);
+    printf("%s  versionPtp: 0x%x\n", prefix, hdr->versionPtp);
+    printf("%s  minorVersionPtp: 0x%x\n", prefix, hdr->minorVersionPtp);
+    printf("%s  messageLength: 0x%0x\n", prefix, hdr->messageLength);
+    printf("%s  domainNumber: 0x%0x\n", prefix, hdr->domainNumber);
+    printf("%s  minorSdoId: 0x%x\n", prefix, hdr->minorSdoId);
+    printf("%s  flags: 0x%0x%0x\n", prefix, hdr->flags[1], hdr->flags[0]);
+    printf("%s  correctionField: %ld\n", prefix, hdr->correctionField);
+    printf("%s  messageTypeSpecific: 0x%0x\n", prefix, hdr->messageTypeSpecific);
+    printf("%s  clockIdentity: 0x%0lx\n", prefix, clockIdentity);
+    printf("%s  portNumber: 0x%0x\n", prefix, hdr->portIdentity.portNumber);
+    printf("%s  sequenceId: %d\n", prefix, hdr->sequenceId);
+    printf("%s  controlField: 0x%0x\n", prefix, hdr->controlField);
+    printf("%s  logMessagePeriod: %d\n", prefix, hdr->logMessagePeriod);
+}
+
+void print_timestamp(const char* prefix, struct Timestamp* ts) {
+    uint64_t secs = (uint64_t)ts->seconds_msb << 32 | ts->seconds_lsb;
+    printf("%soriginTimeStamp:\n", prefix);
+    printf("%s  secs: %ld\n", prefix, secs);
+    printf("%s  nsecs: %d\n", prefix, ts->nanoseconds);
 }
 
 void print_ptp_sync_msg(struct ptp_sync_msg* msg) {
-    printf("ptp_msg: { ");
-    print_ptp_header(&msg->header);
-    uint64_t secs = (uint64_t)msg->originTimeStamp.seconds_msb << 32 | msg->originTimeStamp.seconds_lsb;
-    printf(" originTimeStamp: { secs: %ld nsecs: %d } }\n", secs, msg->originTimeStamp.nanoseconds);
+    printf("ptp_msg:\n");
+    print_ptp_header("  ", &msg->header);
+    print_timestamp("  ", &msg->originTimeStamp);
 }
 
 #endif // __MSG_H__
